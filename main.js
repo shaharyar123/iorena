430a7e5de6f158f92dbf9d673741133cc672a5d0
Haider Ali <haiderali3010@gmail.com>
2018-12-12 01:32:20 +0500

    style slider and add slidebar

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+jQuery(document).ready(function ($) {
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
 
- $(document).ready(function () {
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        focusOnSelect: true
-      });
-     
-      $('a[data-slide]').click(function(e) {
-        e.preventDefault();
-        var slideno = $(this).data('slide');
-        $('.slider-nav').slick('slickGoTo', slideno - 1);
-      });  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true,
+    // centerMode: true,
+  });
 
-      
+  // $('a[data-slide]').click(function (e) {
+  //   e.preventDefault();
+  //   var slideno = $(this).data('slide');
+  //   $('.slider-nav').slick('slickGoTo', slideno - 1);
+  // });
+
+  $("#room").on('click', function () {
+    $('.main-slider-carousel-wrapper').toggleClass("slider-slide-up");
+  });
+
+  $("#slide-bar-arrow").on('click', function () {
+    $('.slide-bar-wrapper').toggleClass("side-bar-show");
+  });
+
+  $( "#accordion" ).accordion();
+});
